5850d85ffbe5a0b99ce431ff4b8f62492985f2a7
clem2tem <cldetemmerman@icloud.com>
2025-09-10 13:49:50 +0200

    refactor: remove unused lighting effect logic from Desk component

--- src/components/Desk.tsx
+++ src/components/Desk.tsx
@@ -1,26 +1,8 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useGLTF } from '@react-three/drei'
-import * as THREE from 'three'
 
 const Desk: React.FC = () => {
     const gltf = useGLTF('models/Desk/Desk.glb')
-
-    useEffect(() => {
-        gltf.scene.traverse((child: any) => {
-            if (child.isMesh && child.name.includes('KLight')) {
-                // Création d'une vraie lumière sous chaque touche
-                const light = new THREE.PointLight(0x00ffcc, 3, 0.7) // couleur, intensité, distance
-                // Positionner la lumière légèrement en dessous de la touche
-                const bbox = new THREE.Box3().setFromObject(child)
-                const center = bbox.getCenter(new THREE.Vector3())
-                const max = bbox.max
-                light.position.set(center.x, max.y, center.z)
-                
-
-                child.parent.add(light) // ajouter la lumière dans la même hiérarchie
-            }
-        })
-    }, [gltf])
 
 
     return (
